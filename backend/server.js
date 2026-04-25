71b985b381f65e45ebcda2af2ac0d273185d732e
Ravi Kiran <allimilliravikiran@gmail.com>
2026-04-25 17:56:02 +0530

    build frontend dashboard with metrics display, insights and suggestions

--- backend/server.js
+++ backend/server.js
@@ -10,86 +10,87 @@ const app = express();
 app.use(cors());
 
 app.get("/metrics", (req, res) => {
-  const { developer, month } = req.query;
+    
+    const { developer, month } = req.query;
+    
+    const issues = jiraData.filter( d => d.developer_id === developer && d.month === month );
+    
+    const pr = prData.filter( d => d.developer_id === developer && d.month === month);
+    
+    const deployment = deploymentData.filter(d => d.developer_id === developer && d.month === month);
+    
+    const bug = bugData.filter( d => d.developer_id === developer && d.month === month && d.escaped === true);
+    console.log(bug);
+    
+    
+    const cycleTime = issues.reduce((s, i) => s + i.cycle_time_days, 0) /(issues.length || 1);
+    
+    const leadTime = deployment.reduce((s, i) => s + i.lead_time_days, 0) /(deployment.length || 1);
+    
+    const deployCount = deployment.length;
+    const bugRate = (bug.length / (issues.length || 1)) * 100;
+    const prCount = pr.length;
+    const insights = [];
+    const suggestions = [];
+    
+    if (bugRate > 20) {
+        insights.push("High bug rate — quality issue");
+        suggestions.push("Improve testing before release");
+    } else if (bugRate > 0) {
+        insights.push("Moderate bug rate — monitor quality");
+    } else {
+        insights.push("Excellent quality — no bugs");
+    }
 
-  const issues = jiraData.filter( d => d.developer_id === developer && d.month === month );
+    
+    if (cycleTime > 5) {
+        insights.push(" Work taking longer than expected");
+        suggestions.push("Break tasks into smaller tickets");
+    } else if (cycleTime > 3) {
+        insights.push("Cycle time is acceptable");
+    } else {
+        insights.push("Fast development speed");
+    }
 
-  const pr = prData.filter( d => d.developer_id === developer && d.month === month);
+    if (leadTime > 4) {
+        insights.push("Delay in reaching production");
+        suggestions.push("Reduce PR size or speed up reviews");
+    } else if (leadTime > 2) {
+        insights.push("Lead time is reasonable");
+    } else {
+        insights.push("Fast delivery to production");
+    }
 
-  const deployment = deploymentData.filter(d => d.developer_id === developer && d.month === month);
+    if (deployCount < 2) {
+        insights.push("Low deployment frequency");
+        suggestions.push("Increase deployment frequency");
+    } else if (deployCount <= 4) {
+        insights.push("Deployment frequency is good");
+    } else {
+        insights.push("High deployment frequency");
+    }
+    
+    if (prCount < 2) {
+        insights.push("Low PR throughput");
+        suggestions.push("Increase smaller PRs");
+    } else if (prCount <= 4) {
+        insights.push("PR flow is steady");
+    } else {
+        insights.push("Too many small PRs");
+        suggestions.push("Combine related changes");
+    }
 
-  const bug = bugData.filter( d => d.developer_id === developer && d.month === month);
-
-  const cycleTime = issues.reduce((s, i) => s + i.cycle_time_days, 0) /(issues.length || 1);
-
-  const leadTime = deployment.reduce((s, i) => s + i.lead_time_days, 0) /(deployment.length || 1);
-
-  const deployCount = deployment.length;
-
-  const bugRate = (bug.length / (issues.length || 1)) * 100;
-
-  const prCount = pr.length;
-
-  const insights = [];
-  const suggestions = [];
-
-  if (bugRate > 20) {
-    insights.push("High bug rate — quality issue");
-    suggestions.push("Improve testing before release");
-  } else if (bugRate > 0) {
-    insights.push("Moderate bug rate — monitor quality");
-  } else {
-    insights.push("Excellent quality — no bugs");
-  }
-
-  if (cycleTime > 5) {
-    insights.push(" Work taking longer than expected");
-    suggestions.push("Break tasks into smaller tickets");
-  } else if (cycleTime > 3) {
-    insights.push("Cycle time is acceptable");
-  } else {
-    insights.push("Fast development speed");
-  }
-
-  if (leadTime > 4) {
-    insights.push("Delay in reaching production");
-    suggestions.push("Reduce PR size or speed up reviews");
-  } else if (leadTime > 2) {
-    insights.push("Lead time is reasonable");
-  } else {
-    insights.push("Fast delivery to production");
-  }
-
-  if (deployCount < 2) {
-    insights.push("Low deployment frequency");
-    suggestions.push("Increase deployment frequency");
-  } else if (deployCount <= 4) {
-    insights.push("Deployment frequency is good");
-  } else {
-    insights.push("High deployment frequency");
-  }
-
-  if (prCount < 2) {
-    insights.push("Low PR throughput");
-    suggestions.push("Increase smaller PRs");
-  } else if (prCount <= 4) {
-    insights.push("PR flow is steady");
-  } else {
-    insights.push("Too many small PRs");
-    suggestions.push("Combine related changes");
-  }
-
-  res.json({
-    developer,
-    month,
-    leadTime: Number(leadTime.toFixed(1)),
-    cycleTime: Number(cycleTime.toFixed(1)),
-    prCount,
-    deployCount,
-    bugRate: Number(bugRate.toFixed(1)),
-    insights,
-    suggestions
-  });
+    res.json({
+        developer,
+        month,
+        leadTime: Number(leadTime.toFixed(1)),
+        cycleTime: Number(cycleTime.toFixed(1)),
+        prCount,
+        deployCount,
+        bugRate,
+        insights,
+        suggestions
+    });
 });
 
 app.listen(4000, () => console.log("Server running on 4000"));
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,122 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [developer, setDeveloper] = useState("DEV-002");
+  const [month, setMonth] = useState("2026-04");
+  const [data, setData] = useState(null);
+
+  const getMetrics = async () => {
+    const res = await axios.get(`http://localhost:4000/metrics?developer=${developer}&month=${month}`);
+    setData(res.data);
+  };
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+    <div className="min-h-screen bg-gray-100 p-6">
+      <div className="max-w-3xl mx-auto bg-white p-6 rounded-xl shadow">
+
+        <h1 className="text-2xl font-bold mb-4 text-center">
+          Developer Productivity Dashboard
+        </h1>
+
+        <div className="flex gap-3 mb-4">
+          <select
+            className="border p-2 rounded w-full"
+            onChange={(e) => setDeveloper(e.target.value)}
+          >
+            <option value="DEV-001">Ava Chen</option>
+            <option value="DEV-002">Noah Patel</option>
+            <option value="DEV-003">Mia Lopez</option>
+            <option value="DEV-004">Lucas Reed</option>
+            <option value="DEV-005">Emma Roy</option>
+            <option value="DEV-006">Ishan Mehta</option>
+            <option value="DEV-008">Owen Brooks</option>
+          </select>
+
+          <select
+            className="border p-2 rounded w-full"
+            onChange={(e) => setMonth(e.target.value)}
+          >
+            <option value="2026-03">March</option>
+            <option value="2026-04">April</option>
+          </select>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
+
         <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
+          onClick={getMetrics}
+          className="w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700"
         >
-          Count is {count}
+          Get Metrics
         </button>
-      </section>
 
-      <div className="ticks"></div>
+        {data && (
+          <div className="mt-6">
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+            <div className="grid grid-cols-2 gap-4 mb-6">
+              <div className="bg-gray-50 p-4 rounded shadow">
+                <p className="text-sm text-gray-500">Lead Time</p>
+                <p className="text-xl font-semibold">{data.leadTime}</p>
+              </div>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+              <div className="bg-gray-50 p-4 rounded shadow">
+                <p className="text-sm text-gray-500">Cycle Time</p>
+                <p className="text-xl font-semibold">{data.cycleTime}</p>
+              </div>
+
+              <div className="bg-gray-50 p-4 rounded shadow">
+                <p className="text-sm text-gray-500">PR Count</p>
+                <p className="text-xl font-semibold">{data.prCount}</p>
+              </div>
+
+              <div className="bg-gray-50 p-4 rounded shadow">
+                <p className="text-sm text-gray-500">Deployments</p>
+                <p className="text-xl font-semibold">{data.deployCount}</p>
+              </div>
+
+              <div className="bg-gray-50 p-4 rounded shadow col-span-2">
+                <p className="text-sm text-gray-500">Bug Rate</p>
+                <p className="text-xl font-semibold text-red-500">
+                  {data.bugRate}%
+                </p>
+              </div>
+            </div>
+
+            <div className="mb-4">
+              <h2 className="font-semibold mb-2">Insights</h2>
+              {data.insights.map((i, idx) => (
+                <p key={idx} className="text-sm text-gray-700">
+                   {i}
+                </p>
+              ))}
+            </div>
+
+
+            <div>
+              <h2 className="font-semibold mb-2">Suggestions</h2>
+              {data.suggestions.map((s, idx) => (
+                <p key={idx} className="text-sm text-gray-700">
+                   {s}
+                </p>
+              ))}
+            </div>
+
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
